cb9cd1fbc261c2c580549ef001c8b5d26961bd70
MedBaya2024 <166258233+MedBaya2024@users.noreply.github.com>
2024-06-23 05:15:37 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const navMenu = document.getElementById('navMenu');
-    if (navMenu.style.display === 'flex') {
-        navMenu.style.display = 'none';
-    } else {
-        navMenu.style.display = 'flex';
-    }
-}
