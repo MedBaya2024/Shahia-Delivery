a989013011869cab30d757713153809b9aac55ed
MedBaya2024 <166258233+MedBaya2024@users.noreply.github.com>
2024-06-23 05:22:53 +0200

    Commit Changes

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    if (navMenu.style.display === 'flex') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'flex';
+    }
+}
